679f87f11f9354e5f1c31c121ff9ed9bb731c1ad
Tan Phan <duytan.phanle@gmail.com>
2019-07-11 01:57:18 +0700

    Add SignIn

---
+++ src/components/sign-in/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import FormInput from '../form-input';
+import CustomButton from '../custom-button';
+
+import './styles.scss';
+
+class SignIn extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ email: '', password: '' });
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your emai and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            label="Email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            autoComplete="username"
+            required
+          />
+          <FormInput
+            name="password"
+            type="password"
+            label="Password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            autoComplete="current-password"
+            required
+          />
+          <CustomButton type="submit">Sign in</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
