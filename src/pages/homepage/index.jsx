5bd3b845c104619feeeeb1b4cc838852fe3ef6b1
Tan Phan <duytan.phanle@gmail.com>
2019-07-10 18:21:51 +0700

    Broken HomePage to smaller components && Finished styling

---
+++ src/pages/homepage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Directory from '../../components/directory';
+
+import './styles.scss';
+
+const HomePage = () => (
+  <div className="home-page">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
