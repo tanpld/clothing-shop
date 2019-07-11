3d47e84e4f07413576d8ec5b4bc9346351f29d25
Tan Phan <duytan.phanle@gmail.com>
2019-07-11 19:17:35 +0700

    Add sign up with email and password

---
+++ src/components/sign-up/index.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+
+import FormInput from '../form-input';
+import CustomButton from '../custom-button';
+
+import './styles.scss';
+
+class SignUp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      displayName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    };
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    const { displayName, email, password, confirmPassword } = this.state;
+
+    if (password !== confirmPassword) {
+      alert("Password don't match!");
+      return;
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password,
+      );
+      createUserProfileDocument(user, { displayName });
+
+      e.target.reset(); // clear form
+      this.setState({
+        displayName: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  render() {
+    const { displayName, email, password, confirmPassword } = this.state;
+    return (
+      <div className="sign-up">
+        <h2 className="title">I do not account</h2>
+        <span>Sign up with your email</span>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="displayName"
+            label="Display Name"
+            value={displayName}
+            handleChange={this.handleChange}
+            autoComplete="username"
+            required
+          />
+          <FormInput
+            type="text"
+            name="email"
+            label="Email"
+            value={email}
+            handleChange={this.handleChange}
+            autoComplete="username"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            label="Password"
+            value={password}
+            handleChange={this.handleChange}
+            required
+          />
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            label="Confirm Password"
+            value={confirmPassword}
+            handleChange={this.handleChange}
+            required
+          />
+          <CustomButton type="submit">SIGN UP</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
--- src/pages/signin-signup/index.jsx
+++ src/pages/signin-signup/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in';
+import SignUp from '../../components/sign-up';
 
 import './styles.scss';
 
 const SignInSignUp = () => (
-  <div className="sign-in-and-sign-up">
+  <div className="sign-in-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
