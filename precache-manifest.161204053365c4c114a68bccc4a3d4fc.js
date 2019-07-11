eafc4fa8950a7d3aebdd433b67ba07bf1582877c
Tan Phan <duytan.phanle@gmail.com>
2019-07-11 19:50:24 +0700

    Updates

---
+++ precache-manifest.161204053365c4c114a68bccc4a3d4fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0699146d85c2a63a8b06632a4a08f27",
+    "url": "/clothing-shop/index.html"
+  },
+  {
+    "revision": "4c9f645882e0ceef3ecd",
+    "url": "/clothing-shop/static/css/main.c60f2e6a.chunk.css"
+  },
+  {
+    "revision": "b43bb2603c35ab7b9d21",
+    "url": "/clothing-shop/static/js/2.dc74a86a.chunk.js"
+  },
+  {
+    "revision": "4c9f645882e0ceef3ecd",
+    "url": "/clothing-shop/static/js/main.70cf67cf.chunk.js"
+  },
+  {
+    "revision": "ff03d3b57b366662af98",
+    "url": "/clothing-shop/static/js/runtime~main.1ffd74ad.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/clothing-shop/static/media/crown.ed28accb.svg"
+  }
+]);
