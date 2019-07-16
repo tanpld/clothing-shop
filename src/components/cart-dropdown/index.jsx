feba985c5e50d0377e273a84b13ed2aaea6974d1
Tan Phan <duytan.phanle@gmail.com>
2019-07-16 15:10:37 +0700

    Use reselect for cart selectors (memozied)

--- src/components/cart-dropdown/index.jsx
+++ src/components/cart-dropdown/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import CartItem from '../cart-item';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
+import CartItem from '../cart-item';
 import CustomButton from '../custom-button';
 
 import './styles.scss';
@@ -18,8 +19,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropdown);
