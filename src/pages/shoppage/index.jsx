fc0b83ed7344d72efe226f8c24a28fa50e791368
Tan Phan <duytan.phanle@gmail.com>
2019-07-28 12:44:01 +0700

    Move collections state to Redux

--- src/pages/shoppage/index.jsx
+++ src/pages/shoppage/index.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectCollections } from '../../redux/shop/shop.selectors';
 
 import PreviewCollection from '../../components/preview-collection';
 
-import SHOP_DATA from './shop.data';
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...props }) => (
+      <PreviewCollection key={id} {...props} />
+    ))}
+  </div>
+);
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...props }) => (
-          <PreviewCollection key={id} {...props} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -36,7 +36,7 @@ const INITIAL_STATE = {
 };
 
 const directoryReducer = (state = INITIAL_STATE, action) => {
-  switch (action) {
+  switch (action.type) {
     default:
       return state;
   }
