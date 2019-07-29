4c97d9a46f208e84825f2f672df6895a4c7c5b00
Tan Phan <duytan.phanle@gmail.com>
2019-07-29 13:09:05 +0700

    Add new selectors

--- src/components/collection-overview/index.jsx
+++ src/components/collection-overview/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import PreviewCollection from '../preview-collection';
 
@@ -17,7 +17,7 @@ const CollectionOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
---
+++ src/pages/collection/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import CollectionItem from '../../components/collection-item';
+
+import './styles.scss';
+
+const CollectionPage = ({ collection }) => (
+  <div className="collection-page">
+    <h2 className="title">{collection.title}</h2>
+    <div className="items">
+      {collection.items.map(item => (
+        <CollectionItem key={item.id} item={item} />
+      ))}
+    </div>
+  </div>
+);
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
